54fc6527a288c0f32dffda0fa0d0c4ea6475b83b
odusdegreat <tobiodubote@gmail.com>
2026-09-22 23:13:13 +0100

    omo mehn

--- scripts/start-standalone.mjs
+++ scripts/start-standalone.mjs
@@ -2,4 +2,7 @@
 // must listen on every interface, while retaining the platform-provided PORT.
 process.env.HOSTNAME = "0.0.0.0";
 
+console.log("PORT:", process.env.PORT);
+console.log("HOSTNAME:", process.env.HOSTNAME);
+
 await import("../.next/standalone/server.js");
